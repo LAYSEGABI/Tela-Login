0d0ff0d957902843e789e91cab35a59de8389b70
LAYSEGABI <laysegabrielly13@gmail.com>
2024-03-28 12:14:00 -0300

    teste de commit

---
+++ testeTalvez/src/main/java/com/mycompany/testetalvez/TesteTalvez.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testetalvez;
+
+/**
+ *
+ * @author User
+ */
+public class TesteTalvez {
+
+    public static void main(String[] args) {
+        System.out.println("Hello pra ver se funciona!");
+    }
+}
